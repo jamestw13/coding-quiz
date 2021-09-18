4a7b41fbb29ded6259bcf0ff5cafc63035fde139
TJ James <jamestw13@gmail.com>
2021-09-18 15:39:53 -0500

    added Q/A content builder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,44 @@
-const QUIZ_ARRAY = [{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""}];
+const QUIZ_ARRAY = [{question: "Question 1",answer: ["true", "false"]},{question: "Question 2",answer: ["true", "false1", "false2"]},{question: "Question 3",answer: ["true", "false1","false2","false3"]}]
 let mainContent = document.getElementById("page-content");
+let timeScore = 100;
+let questionNumber = 0;
 
-let runQuiz = function() {
-  alert("pressed")
+let createQuestionPage = function (questionObject) {
+  // Clear last page
+  while (mainContent.firstChild){
+    mainContent.removeChild(mainContent.firstChild);
+  }
+
+  // Create question page
+  let questionPage = document.createElement("div");
+
+  let questionText = document.createElement("h2");
+  questionText.textContent = questionObject.question;
+
+  questionPage.appendChild(questionText);
+
+  for (var j = 0; j < questionObject.answer.length; j++) {
+    let answerText = document.createElement("button");
+    answerText.textContent = questionObject.answer[j];
+
+    questionPage.appendChild(answerText);
+  }
+
+  mainContent.appendChild(questionPage);
+
 }
 
+let runQuiz = function() {
+
+  // Start Timer Countdown
+
+  createQuestionPage(QUIZ_ARRAY[questionNumber]);
+    
+
+  
+}
+
+// Create and display invitation to start the quiz
 let initiateContent = function () {
   let welcomePage = document.createElement("div");
   
