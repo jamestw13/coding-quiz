9500c1293addfb8ac2523ddd7de6d215ce3e9c87
TJ James <jamestw13@gmail.com>
2021-09-18 14:02:52 -0500

    created structure of quiz start content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 const QUIZ_ARRAY = [{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""},{question: "",answer: ""}];
+let mainContent = document.getElementById("page-content");
+
+let runQuiz = function() {
+  alert("pressed")
+}
+
+let initiateContent = function () {
+  let welcomePage = document.createElement("div");
+  
+  let welcomeText = document.createElement("h2");
+  welcomeText.textContent = "Press the button to start the quiz."
+
+  let startButton = document.createElement("button");
+  startButton.addEventListener("click", runQuiz);
+  
+  welcomePage.appendChild(welcomeText);
+  welcomePage.appendChild(startButton);
+
+  mainContent.appendChild(welcomePage);
+}
+
+initiateContent();
